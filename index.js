b36c36932fec767b0db0ea845bafb890208d72d9
KaiserX <wiz0isaac@gmail.com>
2023-06-29 11:54:39 +0100

    edited

---
+++ index.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".food-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+  });
